e5dfc734cc7aa9cafc57f7e6c63e73f170265679
Nkaleth <nseguralu@gmail.com>
2024-04-05 18:28:24 -0500

    REFACTOR getPosts controller to use data from mongoDB

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,18 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/lovecraft.jpg",
-        creator: {
-          name: "Nilton",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetched posts successfully", posts: posts });
+    })
+    .catch((error) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
