7903e9fc6e19068c1dc9366a36a24e01b9973cd8
Nkaleth <nseguralu@gmail.com>
2024-04-05 12:54:45 -0500

    ADD or condition to send error 500 if the status is undefined

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/feed", feedRoutes);
 
 app.use((error, req, next) => {
   console.log(error);
-  const status = error.statusCode;
+  const status = error.statusCode || 500;
   const message = error.message;
   res.status(status).json({ message: message });
 });
