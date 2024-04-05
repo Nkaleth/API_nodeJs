95ffb34e38abb6d587d7424ea7692ccc52730839
Nkaleth <nseguralu@gmail.com>
2024-04-05 12:27:09 -0500

    ADD functionality to store posts in the database also images url with mocked data for now

--- controllers/feed.js
+++ controllers/feed.js
@@ -29,16 +29,22 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title, content);
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Nilton Segura" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/lovecraft.jpg",
+    creator: { name: "Nilton Segura" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
