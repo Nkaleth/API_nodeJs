ad3b1b1e8e1c60ab2880114f48f766887ae82a1a
Nkaleth <nseguralu@gmail.com>
2024-04-05 18:39:04 -0500

    FIX middle ware for error handling on app.js including the 'res' input

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.use((error, req, next) => {
+app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
