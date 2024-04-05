5664043cf86d28c1ac4ebb90f155316100dae55e
Nkaleth <nseguralu@gmail.com>
2024-04-05 18:23:12 -0500

    ADD status 200, massage and post in json format when a post is found

--- controllers/feed.js
+++ controllers/feed.js
@@ -60,6 +60,7 @@ exports.getPost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      res.status(200).json({ message: "Post fetched.", post: post });
     })
     .catch((err) => {
       if (!err.statusCode) {
